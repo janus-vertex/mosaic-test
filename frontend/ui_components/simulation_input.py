b2704b8f9009c4341b5df6a686cfcd67ef894095
kyliow <kongyouliow@gmail.com>
2025-05-07 23:11:32 +0800

    default value for pick

--- frontend/ui_components/simulation_input.py
+++ frontend/ui_components/simulation_input.py
@@ -58,10 +58,10 @@ class SimulationInputUI:
         streamlit.write("#### Peak throughput per station")
         col1, col2 = streamlit.columns(2)
         pick_throughput = col1.number_input(
-            "Pick throughput (bins/h)", min_value=1, value=100
+            "Pick (outbound) throughput (bins/h)", min_value=1, value=100
         )
         goods_in_throughput = col2.number_input(
-            "Goods-in throughput (bins/h)", min_value=1, value=100
+            "Goods-in (inbound) throughput (bins/h)", min_value=1, value=100
         )
 
         recommended_number_of_skycars = self._recommend_number_of_skycars(
